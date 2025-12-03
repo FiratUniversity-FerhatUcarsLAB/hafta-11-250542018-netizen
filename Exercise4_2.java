eb8a3eaf173fe74f8119e9a427ff72534b1e13f2
250542018-netizen <250542018@firat.edu.tr>
2025-12-03 18:10:35 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -1,23 +1,29 @@
+/*
+ * Ad Soyad: Akın Ağaçbacak
+ * Ogrenci No: 250542018
+ * Tarih: 03/12/2025
+ * Aciklama: Exercise 4_2
+ *
+ */
+
 public class Exercise4_2 {
 
-  // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
-
     public static void main(String[] args) {
-        zippo("rattle", 13);
+        zippo("rattle", 13); // 1. Başlangıç: zippo metodu çağrılır (quince="rattle", flag=13).
     }
 
     public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
+        System.out.println(blimp);      // 5. Ekrana "rattle" yazılır.
+        zippo("ping", -5);              // 6. zippo tekrar çağrılır (quince="ping", flag=-5).
     }
 
     public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
+        if (flag < 0) {                 // 2. Kontrol (13 < 0 mı? Hayır). | 7. Kontrol (-5 < 0 mı? Evet).
+            System.out.println(quince + " zoop"); // 8. Ekrana "ping zoop" yazılır.
         } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+            System.out.println("ik");             // 3. Ekrana "ik" yazılır.
+            baffle(quince);                       // 4. baffle metodu çağrılır (blimp="rattle").
+            System.out.println("boo-wa-ha-ha");   // 9. baffle bitti, buraya dönüldü. Ekrana "boo-wa-ha-ha" yazılır.
         }
     }
 }
